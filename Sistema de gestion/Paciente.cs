6ced25d3696643f28881009a0ac9529fd6ad9f21
Alberto Garcia <flacogg12@gmail.com>
2025-08-24 15:37:44 -0600

    Agregamos mas funcionalidada al Modulo de Pacientes

---
+++ Sistema de gestion/Paciente.cs
@@ -0,0 +1,12 @@
+namespace Sistema_Gestion;
+
+public class Paciente
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string Direccion { get; set; }
+    public int Telefono { get; set; }
+    public string Correo { get; set; }
+    public string Historial { get; set; }
+    public string Medico { get; set; }
+}
--- Sistema de gestion/Program.cs
+++ Sistema de gestion/Program.cs
@@ -2,28 +2,53 @@
 
 namespace Sistema_Gestion;
 
+class Paciente()
+{
+    //Datos Personales de los Pacientes 
+    private string Nombre { get; set; }
+    private int Edad { get; set; }
+    private string Direccion { get; set; }
+    private int Telefono { get; set; }
+    private string Correo { get; set; }
+    private string Historial { get; set; }
+    private string Medico { get; set; }
+}
+
 class Sistema_De_Gestion_Para_Una_Clinica_Medica
 {
+    // private List<Paciente> listaPacientes = new List<Paciente>();
+    private List<Paciente> listaPacientes = new List<Paciente>();
 
 
 
 
+    // Modulo de Pacientes
+    public void Alta_Paciente()
+    {
+        
+    }
 
-    static void Main(string[] args)
+
+
+
+    //Modulos 
+    public void Pacientes()
     {
         while (true)
         {
-            Console.WriteLine("BIENVENIDO AL SISTEMA DE GESTION PARA LA CLINICA \"THE MEDIC CENTER\" \n ");
-            Console.WriteLine("Modulos\n ");
-            Console.WriteLine("1.- Modulo de Pacientes\t 2.- Modulo de Medicos\t 3.- Modulo de citas\t 4.- Modulo de Consultas\t 5.- Modulo de Administracion\n");
-            Console.WriteLine("6.- Salir ");
-            int Opcion = int.Parse(Console.ReadLine());
-            break;
-
+            Console.WriteLine();
+            Console.WriteLine("Modulo de Pacientes");
+            Console.WriteLine();
+            Console.WriteLine("1.- Alta de paciente\t 2.- Baja de pacientes\t 3.- Modificacion de datos\t 4.- Regresar al Menu Principal\n");
+            if (!int.TryParse(Console.ReadLine(), out int Opcion))
+            {
+                Console.WriteLine("Opción inválida. Intente de nuevo.");
+                continue;
+            }
             switch (Opcion)
             {
                 case 1:
-
+                    
                     break;
                 case 2:
                     break;
@@ -31,14 +56,75 @@ class Sistema_De_Gestion_Para_Una_Clinica_Medica
                     break;
                 case 4:
                     break;
-                case 5:
-                    break;
-                case 6:
-                    break;
                 default:
+                    Console.WriteLine();
+                    break;
+
 
             }
+        }
 
+    }
+    static void Medicos()
+    {
+               Console.WriteLine("Modulo de Medicos");
+    }
+    static void Citas()
+    {
+               Console.WriteLine("Modulo de Citas");
+    }
+    static void Consultas()
+    {
+               Console.WriteLine("Modulo de Consultas");
+    }
+    static void Administracion()
+    {
+               Console.WriteLine("Modulo de Administracion");
+    }
+
+
+
+
+    static void Main(string[] args)
+        
+    {
+        Sistema_De_Gestion_Para_Una_Clinica_Medica Gestion = new Sistema_De_Gestion_Para_Una_Clinica_Medica( );
+        while (true)
+        {
+            Console.WriteLine("BIENVENIDO AL SISTEMA DE GESTION PARA LA CLINICA \"THE MEDIC CENTER\" \n ");
+            Console.WriteLine("Modulos\n ");
+            Console.WriteLine("1.- Modulo de Pacientes\t 2.- Modulo de Medicos\t 3.- Modulo de citas\t 4.- Modulo de Consultas\t 5.- Modulo de Administracion \t 6.- Salir\n");
+            Console.Write("Opcion: ");
+            if (!int.TryParse(Console.ReadLine(), out int Opcion))
+            {
+                Console.WriteLine("Opción inválida. Intente de nuevo.");
+                continue;
+            }
+
+            switch (Opcion)
+            {
+                case 1:
+                    Gestion.Pacientes();
+                    break;
+                case 2:
+                    Medicos();
+                    break;
+                case 3:
+                    Citas();
+                    break;
+                case 4:
+                    Consultas();
+                    break;
+                case 5:
+                    Administracion();
+                    break;
+                case 6:
+                    Console.WriteLine("Gracias por usar el sistema de gestion de la clinica \"THE MEDIC CENTER\" ");
+                    return;
+                default:
+                    Console.WriteLine("Opción inválida. Intente de nuevo.");
+                    break;
+            }
         }
     }
 }
